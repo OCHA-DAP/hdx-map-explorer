d9a693166f240d44d01d9fa18aa173ca77273d0c
Dan Mihaila <danmihaila@gmail.com>
2016-07-19 16:22:13 +0300

    Update config.constant.js

--- src/app/config.constant.js
+++ src/app/config.constant.js
@@ -1,12 +1,12 @@
 (function(module) {
 
     module.constant("APP_CONFIG", {
-        hxlProxy: "https://data.hdx.rwlabs.org/hxlproxy/data.json?",
+        hxlProxy: "https://data.humdata.org/hxlproxy/data.json?",
         ckanSavePath: "/api/action/powerview_create",
         ckanLoadPath: "/api/action/powerview_show",
         ckanCheckLogin: "/api/action/am_following_user?id=hdx",
-        ckanUrl: "https://data.hdx.rwlabs.org",
-        mapExplorerURL: "https://data.hdx.rwlabs.org/mpx"
+        ckanUrl: "https://data.humdata.org",
+        mapExplorerURL: "https://data.humdata.org/mpx"
     });
 
 }(angular.module("hdx.map.explorer")));
