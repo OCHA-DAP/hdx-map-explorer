f96b86e53b277c3d904de242fd5a9cd607c20e7d
Dan Mihaila <danmihaila@gmail.com>
2016-07-19 16:07:37 +0300

    Update config.constant.js

--- src/app/config.constant.js
+++ src/app/config.constant.js
@@ -1,12 +1,12 @@
 (function(module) {
 
     module.constant("APP_CONFIG", {
-        hxlProxy: "https://feature-data.hdx.rwlabs.org/hxlproxy/data.json?",
+        hxlProxy: "https://test-data.hdx.rwlabs.org/hxlproxy/data.json?",
         ckanSavePath: "/api/action/powerview_create",
         ckanLoadPath: "/api/action/powerview_show",
         ckanCheckLogin: "/api/action/am_following_user?id=hdx",
-        ckanUrl: "https://feature-data.hdx.rwlabs.org",
-        mapExplorerURL: "https://feature-data.hdx.rwlabs.org/mpx"
+        ckanUrl: "https://test-data.hdx.rwlabs.org",
+        mapExplorerURL: "https://test-data.hdx.rwlabs.org/mpx"
     });
 
 }(angular.module("hdx.map.explorer")));
