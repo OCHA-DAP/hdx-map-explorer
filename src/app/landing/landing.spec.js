94ce01406e33bf4c4cd13b983db06ab51fa6abdf
Alexandru Artimon <aalecs@gmail.com>
2016-07-19 18:52:42 +0300

    HDX-4858 - MapEX landing page

---
+++ src/app/landing/landing.spec.js
@@ -0,0 +1,7 @@
+describe('landing section', function () {
+    beforeEach(module('hdx.map.explorer.landing'));
+
+    it('should have a dummy test', inject(function() {
+        expect(true).toBeTruthy();
+    }));
+});
