dc3dbb31be1f90fa7358ce8cdc2254aba5e65454
Dan Mihaila <danmihaila@gmail.com>
2016-08-04 11:01:14 +0300

    staging server config constants

--- src/app/config.constant.js
+++ src/app/config.constant.js
@@ -1,11 +1,11 @@
 (function(module) {
 
     module.constant("APP_CONFIG", {
-        hxlProxy: "https://data.humdata.org/hxlproxy/data.json?",
+        hxlProxy: "https://test-data.humdata.org/hxlproxy/data.json?",
         ckanSavePath: "/api/action/powerview_create",
         ckanCheckLogin: "/api/action/am_following_user?id=hdx",
-        ckanUrl: "https://data.humdata.org",
-        mapExplorerURL: "https://data.humdata.org/mpx"
+        ckanUrl: "https://test-data.humdata.org",
+        mapExplorerURL: "https://test-data.humdata.org/mpx"
     });
 
 }(angular.module("hdx.map.explorer")));
