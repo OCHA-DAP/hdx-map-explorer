0f947dd45155bd4e6273f3db11731fbb2b43cf7d
Dan Mihaila <danmihaila@gmail.com>
2017-05-10 11:56:59 +0300

    fixing https tiles

--- src/app/util/base-layers.js
+++ src/app/util/base-layers.js
@@ -3,22 +3,22 @@
         var ret = {};
         ret.get = function(){
             return {
-                OpenStreetMap_BlackAndWhite: L.tileLayer('http://{s}.tiles.wmflabs.org/bw-mapnik/{z}/{x}/{y}.png', {
+                OpenStreetMap_BlackAndWhite: L.tileLayer('//{s}.tiles.wmflabs.org/bw-mapnik/{z}/{x}/{y}.png', {
                     maxZoom: 18,
                     attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
                 }),
-                OpenStreetMap_HOT: L.tileLayer('http://{s}.tile.openstreetmap.fr/hot/{z}/{x}/{y}.png', {
+                OpenStreetMap_HOT: L.tileLayer('//{s}.tile.openstreetmap.fr/hot/{z}/{x}/{y}.png', {
                     maxZoom: 19,
                     attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>, Tiles courtesy of <a href="http://hot.openstreetmap.org/" target="_blank">Humanitarian OpenStreetMap Team</a>'
                 }),
-                Thunderforest_TransportDark: L.tileLayer('http://{s}.tile.thunderforest.com/transport-dark/{z}/{x}/{y}.png', {
+                Thunderforest_TransportDark: L.tileLayer('//{s}.tile.thunderforest.com/transport-dark/{z}/{x}/{y}.png', {
                     attribution: '&copy; <a href="http://www.opencyclemap.org">OpenCycleMap</a>, &copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>',
                     maxZoom: 19
                 }),
-                Thunderforest_Landscape: L.tileLayer('http://{s}.tile.thunderforest.com/landscape/{z}/{x}/{y}.png', {
+                Thunderforest_Landscape: L.tileLayer('//{s}.tile.thunderforest.com/landscape/{z}/{x}/{y}.png', {
                     attribution: '&copy; <a href="http://www.opencyclemap.org">OpenCycleMap</a>, &copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
                 }),
-                OpenMapSurfer_Grayscale: L.tileLayer('http://korona.geog.uni-heidelberg.de/tiles/roadsg/x={x}&y={y}&z={z}', {
+                OpenMapSurfer_Grayscale: L.tileLayer('//korona.geog.uni-heidelberg.de/tiles/roadsg/x={x}&y={y}&z={z}', {
                     maxZoom: 19,
                     attribution: 'Imagery from <a href="http://giscience.uni-hd.de/">GIScience Research Group @ University of Heidelberg</a> &mdash; Map data &copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
                 }),
